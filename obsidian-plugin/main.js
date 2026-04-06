8396ea1d4ce2c3cd3c6835d2e49b65b5c3d48d69
Michael Schwab <michael.schwab@adaptx.com>
2026-04-06 16:19:32 -0500

    fix: Minimum plugin with console log only

--- obsidian-plugin/main.js
+++ obsidian-plugin/main.js
@@ -1,16 +1,6 @@
 /**
- * SeferFlow Plugin
+ * SeferFlow - Plain JavaScript Plugin
+ * No dependencies, no APIs, just loads
  */
 
-// Global variable to avoid require errors
-var sefeferflowPlugin = {};
-var app = null;
-
-console.log("[SeferFlow] Plugin loaded v1.0.0");
-
-function sfLogin() {
-  console.log("[SeferFlow] Login button clicked");
-  // In real plugin this would show auth form
-}
-
-window.sefeferflow = { sfLogin };
+console.log("[SeferFlow] Loaded");
